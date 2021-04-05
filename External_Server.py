2fc06fb38e0d57188a15d768af181aabac77cdd4
b-first <brandon.fuerst@gmail.com>
2021-04-05 02:13:17 -0400

    login functionality

--- External_Server.py
+++ External_Server.py
@@ -49,10 +49,39 @@ def logout():
     flash(f'{user} has been logged out!')               # Flash message on next page that the user signed out
     return redirect(url_for("login"))
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    return render_template("index.html", isLoggedIn='user' in session)
+    if request.method == "POST":                                # If data submitted with post method
+        username = request.form['Username']                     # Take the value from the form from the input named nm
+        password_plain = request.form['Password']
+        userData = [username, password_plain]                   # List of new user data to send to DB
+        
+        #isLoginSuccess = authenticate_user(userData, db_info)    # Pass in [username, password_entered] and db connection info
+        response = requests.post(
+            SERVER + '/login_internal',
+            json={
+                'username': username,
+                'password_plain': password_plain
+            },
+        )
+        if response.status_code == 200:
+            session['user'] = username                          # Create a session
+            flash(f'{userData[0]} logged in!')                  # Flash message on next page that the user signed in
+            return redirect(url_for("user_page"))               # Redirect to the user page
+        #elif isLoginSuccess is None:
+        #    flash('Incorrect username')
+        else:
+            #flash('Incorrect password')
+            flash('Login failed')
 
+    elif request.method == 'GET' and 'user' in session:         # If a session exists
+        return redirect(url_for("user_page"))                    # Redirect to user page
+    
+    # Only makes it here if
+        # GET while not signed in (no session)
+        # POST but login failed
+    return render_template("login.html", isLoggedIn=False)
+    
 @app.route("/signup", methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
